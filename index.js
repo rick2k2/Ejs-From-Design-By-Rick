5f3faf82f5cc6027d76aef6872e9389774450dce
Rick saha <ricksaha2002@gmail.com>
2023-03-09 12:04:36 +0530

    Add From design EJS

---
+++ index.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const PORT = 3000;
+const app = express();
+app.set("view engine", "ejs");
+app.use(express.static("public"));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.get("/", (req, res) => {
+  res.status(200).render("index", { pageName: "Home Page" });
+});
+app.post("/", (req, res) => {
+  res.status(200).render("Data saved");
+});
+app.get("/login", (req, res) => {
+  res.status(200).render("loginform",{pageName: "Login"});
+});
+app.get("/about", (req, res) => {
+  res.status(200).render("about",{pageName: "About"});
+});
+app.listen(PORT, (err) => {
+  if (!err) {
+    console.log(`App is listening on ${PORT}`);
+  } else {
+    throw err;
+  }
+});
